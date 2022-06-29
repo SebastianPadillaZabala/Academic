e40d8aa49d717b150da22fccc7b3151ea76d249d
Sebastian Padilla <70664928+SebastianPadillaZabala@users.noreply.github.com>
2022-06-29 18:54:36 -0400

    reset-completo12

--- resources/views/livewire/lista-curso.blade.php
+++ resources/views/livewire/lista-curso.blade.php
@@ -25,7 +25,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg>
           </span>
-          <p>{{$c->cantidad_clases}}</p>
+          <p>N° Clases: {{$c->cantidad_clases}}</p>
         </div>
         <div class="flex space-x-1 items-center">
           <span>
